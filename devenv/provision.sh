996d21718c7be9f51b4673a3b2fd1241dfa6853a
Laszlo Ashin <laszlo@ashin.hu>
2020-03-22 15:42:43 +0100

    Implement simple lsusb

--- devenv/provision.sh
+++ devenv/provision.sh
@@ -1,11 +1,15 @@
 #!/bin/sh -uex
 
+sudo dpkg --add-architecture armhf
 sudo apt-get update
 sudo apt-get install --yes \
 	ca-certificates \
 	curl \
 	gcc \
 	gcc-arm-linux-gnueabihf \
+	libusb-1.0-0-dev \
+	libusb-1.0-0-dev:armhf \
+	pkg-config \
 	vim
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
